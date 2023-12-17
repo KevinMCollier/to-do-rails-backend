1eac4dffc48307b6f3bb560c0bd925e3084270d1
kevin collier <collier.kevin.m@gmail.com>
2023-12-17 22:09:08 +0900

    added users controllers and api/v1/base controller

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::BaseController < ActionController::API
+  include Pundit::Authorization
+
+  before_action :log_user
+
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def user_not_authorized(exception)
+    render json: { error: "Unauthorized #{exception.policy.class.to_s.underscore.camelize}.#{exception.query}" }, status: :unauthorized
+  end
+
+  def not_found(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
+
+  def log_user
+    Rails.logger.info "Authorization Header: #{request.headers['Authorization']}"
+    Rails.logger.info "Current User: #{current_user.inspect}"
+  end
+end
---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,24 @@
+module Users
+  class SessionsController < Devise::SessionsController
+    respond_to :json
+
+    private
+
+    def respond_with(resource, _opts = {})
+      if resource.persisted?
+        render json: { user: resource, token: resource.authentication_token }
+      else
+        render json: { error: 'Invalid email or password'}, status: :unauthorized
+      end
+    end
+
+    def respond_to_on_destroy
+      head :no_content
+    end
+
+    # def create
+    #   Rails.logger.info "Received params: #{params.inspect}"
+    #   super
+    # end
+  end
+end
